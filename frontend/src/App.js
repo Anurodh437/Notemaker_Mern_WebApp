22b5219f7bb2b1d728bd9519fb787ada74fd8974
Anurodh437 <anurodhdubey437@gmail.com>
2022-01-18 22:59:28 +0530

    created landing page and navbar with react bootstrap

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,18 @@
-import "./App.css";
+import React from "react";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+import LandingPage from "./screens/LandingPage/LandingPage";
 
-function App() {
-  return <div className="App">
-    Hello World
-  </div>;
-}
+const App = () => {
+  return (
+    <div>
+      <Header />
+      <main>
+        <LandingPage />
+      </main>
+      <Footer />
+    </div>
+  );
+};
 
 export default App;
