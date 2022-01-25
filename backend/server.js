6ea17561d17179abd5e0d94df90c34e7250aa8c7
Anurodh437 <anurodhdubey437@gmail.com>
2022-01-25 13:43:17 +0530

    setup user authentication using mongodb

--- backend/server.js
+++ backend/server.js
@@ -1,26 +1,28 @@
 const express = require('express');
 const notes = require('./data/notes');
 const dotenv = require('dotenv');
-const connectDb = require('./config/db')
+const connectDb = require('./config/db');
+const userRoutes = require('./routes/userRoutes');
+const { notFound, errorHandler } = require('./middlewares/errorMiddleware');
 
 const app = express();
 dotenv.config();
 connectDb();
-
-app.get('/',(req,res) =>{
-    res.send('Api is running');
-});
-
-app.get('/api/notes',(req,res) =>{
-    res.json(notes)
-});
+app.use(express.json())
 
 
+// app.get('/',(req,res) =>{
+//     res.send('Api is running');
+// });
 
-app.get('/api/notes/:id',(req,res) =>{
-    const note = notes.find((n) => n._id === req.params.id);
-    res.send(note);
-});
+// app.get('/api/notes',(req,res) =>{
+//     res.json(notes)
+// });
+
+app.use('/api/users',userRoutes);
+
+app.use(notFound);
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
